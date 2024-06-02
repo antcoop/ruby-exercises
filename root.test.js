17022e6e5cbd6202b1df2b9db7cd4bf6d4236103
Anthony Cooper <iam@anthonycooper.me>
2024-06-02 07:55:23 -0500

    passed tests

--- root.test.js
+++ root.test.js
@@ -7,4 +7,11 @@ describe('Root', () => {
 
     expect(result).toEqual(value);
   });
+
+  test('increments by 1 unless 10', () => {
+    const value = 100;
+    const result = increment(10);
+
+    expect(result).toEqual(value);
+  });
 });
